cff0ca306b239819e2a7f925e969b224c6f1cc0b
rizwanpa11 <rizz.rana@gmail.com>
2020-09-13 01:08:05 +0530

    create Poll changes

--- controllers/Polls.js
+++ controllers/Polls.js
@@ -2,96 +2,127 @@ const { Polls } = require("../models");
 const { PollQuestions } = require("../models");
 const { PollOptions } = require("../models");
 
-const {generateUuid} = require('./../helpers/uuid')
-
+const { generateUuid } = require("./../helpers/uuid");
+const jwt = require("jsonwebtoken");
 
 const createPoll = async (req, res) => {
   try {
     let request = req.body;
-    console.log("CreatePoll--->@@@@@@", request);
     let httpStatusCode;
 
     /* Validations */
-    console.log('+++++++++++++++++++ CONDITIONS',request.poll.status === 'published' && request.questions === undefined );
-    if (!("title" in request.poll)) {
-      console.log('title');
+    if (request === undefined) {
       httpStatusCode = 400;
-      return res.status(400).json({ error: 'Invalid Request', message: 'Poll title is missing.' });
+      return res
+        .status(400)
+        .json({ error: "Invalid Request", message: "Request is missing." });
     }
-    if (!("status" in request.poll)) {
-      console.log('status');
+    if (!("title" in request) || request.title === "") {
       httpStatusCode = 400;
-      return res.status(400).json({ error: 'Invalid Request', message: 'Poll status is missing.' });
+      return res
+        .status(400)
+        .json({ error: "Invalid Request", message: "Poll title is missing." });
     }
-    if(request.poll.status === 'published' && request.questions === undefined){
-      console.log('published and undefined');
+    if (!("status" in request) || request.title === "") {
       httpStatusCode = 400;
-      return res.status(400).json({ error: 'Invalid Request', message: 'Question and its options are mandatory to publish a poll' });
-    }/* else if(request.questions.options === undefined){
-      console.log('else published and option');
-      return res.status(400).json({ error: 'Invalid Request', message: 'Question and its options are mandatory to publish a poll' });
-    } */
-    if(request.poll.status === 'published' && request.questions !== undefined){
-      console.log('inside-- published and !undefined');
-      if(request.questions.length == 0){
-        console.log('inside-- published and length');
-        httpStatusCode = 400;
-        return res.status(400).json({ error: 'Invalid Request', message: 'Question and its options are mandatory to publish a poll' });
+      return res
+        .status(400)
+        .json({ error: "Invalid Request", message: "Poll status is missing." });
+    }
+    if (
+      request.status === "published" &&
+      (request.questions === undefined || request.questions.length == 0)
+    ) {
+      httpStatusCode = 400;
+      return res
+        .status(400)
+        .json({
+          error: "Invalid Request",
+          message: "Question and its options are mandatory to publish a poll"
+        });
+    }
+    if (
+      request.status === "published" &&
+      (request.questions !== undefined || request.questions.length)
+    ) {
+      for (let i = 0; i < request.questions.length; i++) {
+        if (
+          request.questions[i].question === undefined ||
+          request.questions[i].question === null ||
+          request.questions[i].question === ""
+        ) {
+          return res
+            .status(400)
+            .json({
+              error: "Invalid Request",
+              message: "Question title is black."
+            });
+        }
+        if (
+          request.questions[i].options === undefined ||
+          request.questions[i].options.length == 0
+        ) {
+          httpStatusCode = 400;
+          return res
+            .status(400)
+            .json({
+              error: "Invalid Request",
+              message: "Atleast one option is required to publish a poll"
+            });
+          break;
+        }
       }
-    }/* else if(request.questions.options !== undefined){
-      console.log('inside else-- published and !undefined');
-      if(request.questions.options.length == 0){
-        console.log('inside else if-- published and length');
-        httpStatusCode = 400;
-        return res.status(400).json({ error: 'Invalid Request', message: 'Question and its options are mandatory to publish a poll' });
-      }
-    } */
-
-    //check status
-
-    // res.status(httpStatusCode);
-    /* switch (httpStatusCode) {
-      case 400:
-        return res.status(400).json({ error: 'Invalid Request', message: '' });
-        break;
-    } */
+    }
 
     let uuid = generateUuid(6);
-    console.log('UUUUUUUUUUUUUUUU',uuid);
-
-    const poll = await Polls.create({...request.poll,uuid});
+    let token = req.headers.authorization.split(" ")[1];
+    const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    let user_id = decoded.id;
+    let pollParams = {
+      title: request.title || "Title",
+      description: request.description || "",
+      status: request.status || "Draft",
+      start_date: request.start_date || new Date(),
+      end_date: request.end_date || "",
+      uuid,
+      user_id
+    };
+    const poll = await Polls.create(pollParams);
     let { id } = poll;
-    let questions = request.questions.map(val => {
-      return { ...val, poll_id: id };
-    });
+    let questions = [];
+    if(request.questions !== undefined && request.questions.length){
+      questions = request.questions.map(val => {
+        return { ...val, poll_id: id };
+      });
+    }
     let questionRes = [];
-    if(questions !== undefined){
+    if (questions.length) {
       for (let i = 0; i < questions.length; i++) {
         let qRes = await PollQuestions.create(questions[i]);
-        let { options } = questions[i];
-          if(options !== undefined){
+        let { options } = questions[i] || {};
+        if (options !== undefined) {
           options = options.map(val => {
             return { ...val, question_id: qRes.id };
           });
+          let optionRes = await PollOptions.bulkCreate(options);
+          questionRes.push({ ...qRes.dataValues, options: optionRes });
+        }else{
+          questionRes.push({ ...qRes.dataValues, options: [] });
         }
-        let optionRes = await PollOptions.bulkCreate(options);
-        questionRes.push({ ...qRes.dataValues, options: optionRes });
       }
     }
     return res.status(201).json({
       poll,
-      questionRes
+      questions : questionRes
     });
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
 };
 
-
-
 const updatePoll = async (req, res) => {
   try {
-    return res.status(201).send('uodatePoll');
+    return res.status(201).send("uodatePoll");
   } catch (error) {
     return res.status(500).json({ error: error.message });
   }
@@ -105,16 +136,16 @@ const getQuestions = async (req, res) => {
         id: pollId
       }
     });
-    poll = poll[0]
+    poll = poll[0];
     console.log(poll);
     let questions = poll.questions();
     return res.status(200).json({
       questions
     });
   } catch (error) {
-    return res.status(500).json({ error: error.message })
+    return res.status(500).json({ error: error.message });
   }
-}
+};
 
 module.exports = {
   createPoll,
