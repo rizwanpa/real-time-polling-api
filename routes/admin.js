5444f5b02fd1a8f7ca4ee6d9162e443ecff26388
Abhishek Sharma <abhish3kk@gmail.com>
2020-09-12 22:20:05 +0530

    1. remove token from files 2. extract token from jwt token

--- routes/admin.js
+++ routes/admin.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
 var express = require("express");
 var router = express.Router();
+const jwt = require('jsonwebtoken');
 const jwtmiddleware = require('../jwt/jwtmiddleware')
 
 /* GET JWT for authorized user. */
 router.post("/", jwtmiddleware, async function(req, res, next) {
+  let token = req.headers.authorization.split(" ")[1];
+  const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+  let userId = decoded.id;
+  console.log(userId);
   res.json({message:'hello admin'})
 });
 
--- routes/auth.js
+++ routes/auth.js
@@ -5,10 +5,6 @@ const jwt = require('jsonwebtoken');
 var models = require('../models'); 
 var Users = models.Users; 
 
-const ACCESS_TOKEN_SECRET = 'dbnHKWfkhgFXAshcCs5AZ7HUe0dAzDg5DtZZIgn0ZzIUP8CL4h';
-
-const REFRESH_TOKEN_SECRET = 'z01XOWoyEuASSYH9vexE2BzYKvTko1x23tBeeEPMjyMWcAVzlk';
-
 /* GET JWT for authorized user. */
 router.post("/", async function(req, res, next) {
   console.log('================',req.body)
@@ -21,7 +17,16 @@ router.post("/", async function(req, res, next) {
   let user = users[0];
   if(user && user.id) {
     console.log(user.name, user.id);
-    const accessToken = jwt.sign({name: user.name, id:user.id}, process.env.ACCESS_TOKEN_SECRET);
+    const accessToken = jwt.sign(
+      {
+        id:user.id,
+        name: user.name
+      }, 
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: '1d'
+      }
+    );
     console.log('accessToken-->',accessToken);
     res.json({accessToken: accessToken});
   } else {
